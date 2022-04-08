63bf959d5e8a44b1f021742aa74d9366d23c7160
opajkovic <oljaolja170@gmail.com>
2022-04-08 15:09:44 +0200

    added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function myRecursion(arr, n) {
+    var product = 0;
+    for (var i = 0; i < n; i++) {
+        product += arr[i];
+    }
+    return product;
+}
+console.log(myRecursion([1], 0));
+console.log(myRecursion([1, 2, 3, 4], 2));
+console.log(myRecursion([1, 2, 3, 4], 3));
+console.log(myRecursion([1, 2, 3, 4, 5, 6], 5));
+module.exports = myRecursion;
