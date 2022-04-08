072e1e5740a797d886b5a760bb296554a4b3694c
opajkovic <oljaolja170@gmail.com>
2022-04-08 15:48:17 +0200

    added ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+function sumFibonacci(num) {
+        let prevNumber = 0;
+        let currNumber = 1;
+        let result = 0;
+        while (currNumber <= num) {
+          if (currNumber % 2 !== 0) {
+            result += currNumber;
+          }
+          currNumber += prevNumber;
+          prevNumber = currNumber - prevNumber;
+        }
+      
+        return result;
+      } 
+ console.log(sumFibonacci(1));
+ console.log(sumFibonacci(10));
+ console.log(sumFibonacci(20));
+ console.log(sumFibonacci(4));
+ console.log(sumFibonacci(-5));
