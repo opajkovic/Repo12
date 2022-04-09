e15e82b8d455bd76059f305b56884f73e8c35ade
opajkovic <oljaolja170@gmail.com>
2022-04-09 09:08:41 +0200

    added

---
+++ ex07/src/index.js
@@ -0,0 +1,35 @@
+var collection = {
+    2548: {
+        album: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    2468: {
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    1245: {
+        artist: "Robert Palmer",
+        tracks: []
+    },
+    5439: {
+        album: "ABBA Gold"
+    }
+};
+var oldCollection = JSON.parse(JSON.stringify(collection));
+function updateRecords(object, id, prop, value) {
+}
+
+
+console.log(updateRecords(collection, 5439, "artist", "ABBA"));
+console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
+console.log(updateRecords(collection, 2548, "artist", ""),);
+console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
+module.exports = updateRecords;
